266dbcc3121488d918346150cc6f7883fba349b4
genelocated <genelocated@foxmail.com>
2018-04-22 08:37:20 +0800

    Add beetle animation

---
+++ wechat/pages/console/console.js
@@ -0,0 +1,81 @@
+// pages/console/console.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.animation = wx.createAnimation({
+      duration: 200,
+      timingFunction: 'ease',
+      transformOrigin: 'center'
+    })
+  },
+
+  beetleAni: function (info) {
+    if (info.target.id === 'button-left')
+      this.animation.rotate(-45).step()
+    else
+      this.animation.rotate(45).step()
+    this.animation.rotate(0).step()
+    this.setData({
+      //export() 会清除之前的动画操作
+      animation: this.animation.export()
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
