b9f1e081367902bcfc82042c231aecebc533cfd3
genelocated <genelocated@foxmail.com>
2018-04-28 21:00:52 +0800

    Firmware init

---
+++ avr/controlled/main.c
@@ -0,0 +1,18 @@
+/*
+ * controlled.c
+ *
+ * Created: 2018/4/28 20:59:13
+ * Author : geneLocated
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
