1a3d303e53b782c99f5449d8c98a18f579f51965
genelocated <genelocated@foxmail.com>
2018-07-26 02:47:19 +0800

    Add copy link

--- wechat/pages/credits/credits.js
+++ wechat/pages/credits/credits.js
@@ -6,10 +6,25 @@ Page({
    */
   data: {},
 
-  showLink: function (text) {
-    wx.showToast({
-      title: text.target.id,
-      icon: 'none'
+  clickLink: function (text) {
+    wx.setClipboardData({
+      data: text.target.id,
+      fail: function () {
+        wx.showModal({
+          title: '链接复制失败',
+          content: '请检查微信的权限。这个链接是 ' + text.target.id,
+          showCancel: false,
+          confirmText: '好的'
+        })
+      },
+      success: function () {
+        wx.showModal({
+          title: '链接已复制到你的剪贴板',
+          content: '请在浏览器中粘贴访问，' + text.target.id,
+          showCancel: false,
+          confirmText: '好的'
+        })
+      }
     })
   }
 })
